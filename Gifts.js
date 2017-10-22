32ba8dadfae357585e33b98901ffb06115399b9d
Ryan Morton <morton.ryan92@gmail.com>
2017-10-22 11:40:36 -0700

    Styling with additional attributes

--- Gifts.js
+++ Gifts.js
@@ -2,14 +2,24 @@ export default function () {
   const gifts = [
     {
       "name": "Sailor Pro Gear Slim Four Seasons",
+      "category": "Pen",
       "url": "https://www.jetpens.com/Sailor-Pro-Gear-Slim-Shiki-Oriori-Four-Seasons-Fountain-Pen-Manyou-Metallic-Green-Medium-Fine-Nib/pd/16788",
-        "price": 150
+      "price": 150
+    },
+    {
+      "name": "D&D Dice",
+      "url": "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=d+and+d+dice",
+      "notes": "Any dice are cool by me!",
+      "category": "D&D",
+      "price": 150
     },
     {
       "name": "Tomb of Annihilation",
+      "category": "D&D",
       "url": "https://www.amazon.com/Tomb-Annihilation-Dungeons-Dragons-Wizards/dp/0786966106/ref=sr_1_1?ie=UTF8&qid=1508690881&sr=8-1&keywords=tomb+of+annihilation",
       "price": 35
     }
+
   ];
   return gifts;
 }
