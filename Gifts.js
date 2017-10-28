427143ff1e1f381f740383a3c612275caed5685a
Ryan Morton <morton.ryan92@gmail.com>
2017-10-27 18:04:22 -0700

    Updated gift list

--- Gifts.js
+++ Gifts.js
@@ -1,17 +1,11 @@
 export default function () {
   const gifts = [
     {
-      "name": "Sailor Pro Gear Slim Four Seasons",
-      "category": "Pen",
-      "url": "https://www.jetpens.com/Sailor-Pro-Gear-Slim-Shiki-Oriori-Four-Seasons-Fountain-Pen-Manyou-Metallic-Green-Medium-Fine-Nib/pd/16788",
-      "price": 150
-    },
-    {
       "name": "D&D Dice",
       "url": "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=d+and+d+dice",
       "notes": "Any dice are cool by me!",
       "category": "D&D",
-      "price": 150
+      "price": 10
     },
     {
       "name": "Pilot Custom 823",
--- bundle.js
+++ bundle.js
@@ -9635,6 +9635,7 @@ class App extends __WEBPACK_IMPORTED_MODULE_0_react__["Component"] {
         null,
         'Ryan Morton Gift List'
       ),
+      __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(About, null),
       __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_3__List__["a" /* default */], { gifts: this.state.gifts })
     );
   }
@@ -22113,16 +22114,11 @@ module.exports = ReactDOMInvalidARIAHook;
 "use strict";
 /* harmony default export */ __webpack_exports__["a"] = (function () {
   const gifts = [{
-    "name": "Sailor Pro Gear Slim Four Seasons",
-    "category": "Pen",
-    "url": "https://www.jetpens.com/Sailor-Pro-Gear-Slim-Shiki-Oriori-Four-Seasons-Fountain-Pen-Manyou-Metallic-Green-Medium-Fine-Nib/pd/16788",
-    "price": 150
-  }, {
     "name": "D&D Dice",
     "url": "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=d+and+d+dice",
     "notes": "Any dice are cool by me!",
     "category": "D&D",
-    "price": 150
+    "price": 10
   }, {
     "name": "Pilot Custom 823",
     "url": "https://www.jetpens.com/Pilot-Custom-823-Fountain-Pen-Gift-Set-Amber-Brown-Body-Medium-Nib/pd/17086",
